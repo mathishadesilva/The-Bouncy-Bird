944057064ca629e2fc3c8adb3446dcc6c1193991
Mathisha Pinnaduwage <113606267+mathishadesilva@users.noreply.github.com>
2025-12-11 12:27:59 +0530

    Add files via upload

---
+++ BirdScript.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
+        {
+            myRigidbody.linearVelocity = Vector2.up * 10;
+        }
+
+        if (transform.position.y > 17 || transform.position.y < -45)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+              
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
---
+++ PipieMiddleScript.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+
+public class PipieMiddleScript : MonoBehaviour
+{
+    
+    public LogicScript logic;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            logic.addScore(1);
+        }
+         
+    }
+}
